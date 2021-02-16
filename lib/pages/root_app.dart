d5b3746a71e98b2b6888373311c3a9d262c540f2
sabil62 <shresthasabil62@gmail.com>
2021-02-16 21:58:54 +0545

    completed navs

---
+++ lib/pages/root_app.dart
@@ -0,0 +1,73 @@
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
+import 'package:budget/themes/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+
+class RootApp extends StatefulWidget {
+  @override
+  _RootAppState createState() => _RootAppState();
+}
+
+class _RootAppState extends State<RootApp> {
+  int currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("yes"),
+      ),
+      body: getBody(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setIndex(4);
+        },
+        backgroundColor: primary,
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: getAnimatedNavigation(),
+    );
+  }
+
+  Widget getBody() {
+    return IndexedStack(
+      index: currentIndex,
+      children: [
+        Center(child: Text("Daily Page")),
+        Center(child: Text("Stats Page")),
+        Center(child: Text("Create Budget Page")),
+        Center(child: Text("Budget Page")),
+        Center(child: Text("Profile Page")),
+      ],
+    );
+  }
+
+  Widget getAnimatedNavigation() {
+    List<IconData> iconList = [
+      Ionicons.md_calendar,
+      Ionicons.md_stats,
+      Ionicons.md_wallet,
+      Ionicons.ios_person
+    ];
+    return AnimatedBottomNavigationBar(
+      icons: iconList,
+      activeIndex: currentIndex,
+      gapLocation: GapLocation.center,
+      notchSmoothness: NotchSmoothness.smoothEdge,
+      leftCornerRadius: 16,
+      rightCornerRadius: 16,
+      splashColor: secondary,
+      activeColor: primary,
+      inactiveColor: Colors.black.withOpacity(0.5),
+      onTap: (index) {
+        setIndex(index);
+      },
+    );
+  }
+
+  setIndex(index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+}
