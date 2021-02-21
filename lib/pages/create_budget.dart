b53055de7f4b56243844ea702d76260a4755fa66
sabil62 <shresthasabil62@gmail.com>
2021-02-21 21:13:45 +0545

    budget third complete

--- lib/pages/create_budget.dart
+++ lib/pages/create_budget.dart
@@ -9,6 +9,9 @@ class CreateBudget extends StatefulWidget {
 }
 
 class _CreateBudgetState extends State<CreateBudget> {
+  int currentContainer;
+  TextEditingController budget_name = TextEditingController(text: "Shopping");
+  TextEditingController budget_price = TextEditingController(text: "\$720.0");
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -71,43 +74,53 @@ class _CreateBudgetState extends State<CreateBudget> {
                 scrollDirection: Axis.horizontal,
                 itemCount: categories.length,
                 itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
-                    child: Container(
-                      height: 170,
-                      width: (size.width - 90) / 2,
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(12)),
-                      child: Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Container(
-                              height: 38,
-                              width: 38,
-                              decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: Colors.grey.withOpacity(0.3)),
-                              child: Center(
-                                child: Image.asset(
-                                  categories[index]["icon"],
-                                  height: 26,
-                                  width: 26,
+                  return GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        currentContainer = index;
+                      });
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+                      child: Container(
+                        height: 170,
+                        width: (size.width - 90) / 2,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(12),
+                            border: currentContainer == index
+                                ? Border.all(color: primary, width: 2)
+                                : null),
+                        child: Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                height: 38,
+                                width: 38,
+                                decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: Colors.grey.withOpacity(0.3)),
+                                child: Center(
+                                  child: Image.asset(
+                                    categories[index]["icon"],
+                                    height: 26,
+                                    width: 26,
+                                  ),
                                 ),
                               ),
-                            ),
-                            Text(
-                              categories[index]["name"],
-                              style: TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                  letterSpacing: 1.11,
-                                  color: black),
-                            )
-                          ],
+                              Text(
+                                categories[index]["name"],
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    letterSpacing: 1.11,
+                                    color: black),
+                              )
+                            ],
+                          ),
                         ),
                       ),
                     ),
@@ -118,9 +131,74 @@ class _CreateBudgetState extends State<CreateBudget> {
           ),
           SizedBox(
             height: 20,
+          ),
+          //section budget
+          Column(
+            children: [
+              Text(
+                "budget name",
+                style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black.withOpacity(0.5)),
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              TextField(
+                controller: budget_name,
+                style: styler(23.0, 1.0),
+                cursorColor: black,
+                decoration: InputDecoration(
+                    hintText: "Enter your budget",
+                    focusColor: Colors.green,
+                    border: InputBorder.none),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Text(
+                "Total budget",
+                style: styler(18.0, 0.5),
+              ),
+              Row(
+                children: [
+                  TextField(
+                    cursorColor: black,
+                    controller: budget_price,
+                    style: styler(23.0, 1.0),
+                    decoration: InputDecoration(
+                        focusColor: Colors.green,
+                        hintText: "Enter your budget",
+                        border: InputBorder.none),
+                  ),
+                  SizedBox(
+                    width: 22,
+                  ),
+                  Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(9), color: primary),
+                    child: Center(
+                        child: Icon(
+                      AntDesign.arrowright,
+                      color: Colors.white,
+                    )),
+                  )
+                ],
+              )
+            ],
           )
         ],
       ),
     );
+  }
+
+  TextStyle styler(fontSizer, opac) {
+    return TextStyle(
+        fontSize: fontSizer,
+        fontWeight: FontWeight.bold,
+        color: Colors.black.withOpacity(opac));
   }
 }
